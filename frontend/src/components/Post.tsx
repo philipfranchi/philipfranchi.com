dc186288f870e08016178570bc38f4be41df0085
Philip Franchi-Pereira <philipfranchi@gmail.com>
2022-10-07 19:50:36 -0400

    missing post 404s

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,22 +1,31 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams} from "react-router-dom";
 import ReactMarkdown from "react-markdown";
+import Missing from "./Missing";
 
 const Post = () => {
   const postId = useParams().postId;
   const [markdown, setMarkdown] = useState("");
+  const [shouldRedirect, setShouldRedirect] = useState(false);
   useEffect(() => {
     const getPost = async () => {
       await fetch("/api/blog/" + postId)
-        .then((response) => response.json())
-        .then((data) => {
+        .then((response) => {
+          if (response.status == 404) {
+            setShouldRedirect(true)
+          }
+          return response.json()
+        })
+        .then((data) => {            
           setMarkdown(data.body);
           document.title = data.title;
         });
     };
     getPost();
   }, [postId]);
-
+  if (shouldRedirect) {
+    return <Missing />
+  }
   return <ReactMarkdown children={`${markdown}`} />;
 };
 export default Post;
